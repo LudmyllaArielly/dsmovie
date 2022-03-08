6a220318234d3378029118702ca6d33ed291d429
Ludmylla <ludmay4@gmail.com>
2022-03-08 17:54:23 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import Navbar from 'components/Navbar';
+/* Componentes importados servem para configurar a rota */
+import {
+  BrowserRouter, /* aqui deixei a nav que deve ser mostrada em todas as paginas  */
+  Routes, /* vai configurar rota por rota */
+  Route
+} from "react-router-dom";
 
+/* impotandando componentes criados para ativar a rota neles */
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter> 
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
